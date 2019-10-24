c0ed877f24112f99d01b72a76a8fa7c60c927ea5
Krzysztof Wróblewski <k.wroblewski@microsoft.wsei.edu.pl>
2019-10-24 20:04:30 +0200

    Add project files.

---
+++ TicTacToe/Controllers/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TicTacToe.Helpers.AbstractFactory;
+using TicTacToe.Helpers.Builder;
+
+namespace TicTacToe.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Small()
+        {
+            return returnView(MethodBase.GetCurrentMethod().Name.ToLower());
+        }
+        public IActionResult Medium()
+        {
+            return returnView(MethodBase.GetCurrentMethod().Name.ToLower());
+        }
+        private IActionResult returnView(string method)
+        {
+            var director = new Director(BuilderFactory.GetBuilder(method));
+            director.construct();
+            return View(director.GetBoard);
+        }
+    }
+}
---
+++ TicTacToe/Helpers/Builder/Director.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicTacToe.Models;
+
+namespace TicTacToe.Helpers.Builder
+{
+    public class Director
+    {
+        IBoardCreator builder;
+        public Director(IBoardCreator builder)
+        {
+            this.builder = builder;
+        }
+        public void construct()
+        {
+            builder.CreateRows();
+            builder.AddCells();           
+        }
+        public List<Row> GetBoard => builder.Rows;
+
+    }
+}
